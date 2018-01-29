b202760fe7a6307aae6b917d1369c65308b97608
Anthony Robinson <robinsonamk@gmail.com>
2018-01-28 21:41:58 -0800

    merge sort wip

--- lab-anthony/src/merge-sort-mutate.js
+++ lab-anthony/src/merge-sort-mutate.js
@@ -2,8 +2,6 @@
 
 const mergeSortMutate = (items) => {
 
-  let output = [];
-
   if(items.length < 2)
     return items;
 
@@ -12,33 +10,30 @@ const mergeSortMutate = (items) => {
 
   let middle = Math.floor(items.length / 2);
   console.log(middle);
-  let leftSide = mergeSortMutate(items.slice(0, middle));
+  let leftSide = mergeSortMutate(items.splice(0, middle));
   console.log('leftside: ', leftSide);
-  let rightSide = mergeSortMutate(items.slice(middle));
+  let rightSide = mergeSortMutate(items.splice(0));
   console.log('right side: ', rightSide);
 
   while(leftSide.length || rightSide.length){
 
     if(leftSide.length && !rightSide.length){
-      // return items = items.push(leftSide);
-      output = output.concat(leftSide);
-      console.log('output only left side length', output);
+      items.push(...leftSide);
       break;
     }
 
     if(!leftSide.length && rightSide.length){
-      // return items = items.push(rightSide);
-      output = output.concat(rightSide);
-      console.log('output only right side length', output);
+      items.push(...rightSide);
+      console.log('items: ', items);
       break;
     }
 
     if(leftSide[0] <= rightSide[0])
-      output.push(leftSide.shift());
+      items.push(leftSide.shift());
     else
-      output.push(rightSide.shift());
+      items.push(rightSide.shift());
   }
-  return output;
+  return items;
 };
 
 module.exports = mergeSortMutate;
